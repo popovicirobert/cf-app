686aed2ee5755e7caead54615a3630cb99a07574
popovicirobert <popovicirob01@gmail.com>
2020-06-15 18:49:44 +0300

    Modified install

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+
+git clone https://github.com/popovicirobert/cf-app.git
+
 apt-get install python3
-apt-get install pip3
-pip3 install requests
-pip3 install selenium
-pip3 install clipboard
-pip3 install pyvirtualdisplay
+apt-get install python3-pip
+pip install requests
+pip install selenium
+pip install clipboard
+pip install pyvirtualdisplay
+
+cd cf-app
+chmod +x cf-app.sh
+chmod +x checker.sh
+chmod +x make_main.sh
+
+ln -s $PWD/cf-app.sh //usr/local/bin/cf-app
+
+
