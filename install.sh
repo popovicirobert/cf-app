83ca523a78d1103182884a4077b7deac904a176f
popovicirobert <popovicirob01@gmail.com>
2020-06-16 18:08:04 +0300

    Finished installation

--- install.sh
+++ install.sh
@@ -2,18 +2,25 @@
 
 git clone https://github.com/popovicirobert/cf-app.git
 
-sudo -s
-apt-get install python3
-apt-get install python3-pip
-pip install requests
-pip install selenium
-pip install clipboard
-pip install pyvirtualdisplay
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
+pip3 install --upgrade setuptools
+pip3 install requests
+pip3 install selenium
+sudo apt-get install -y xsel xclip
+pip3 install clipboard 
+sudo apt-get install -y python3-pyvirtualdisplay
+
+wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
+tar -xvzf geckodriver*
+chmod +x geckodriver
+sudo mv geckodriver //usr/bin
+rm geckodriver*
 
 cd cf-app
 chmod +x cf-app.sh
 chmod +x checker.sh
 chmod +x make_main.sh
 
-ln -s $PWD/cf-app.sh //usr/local/bin/cf-app
+sudo ln -s $PWD/cf-app.sh //usr/local/bin/cf-app
 cd ..
